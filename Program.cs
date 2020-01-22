75398a2703a4575fb45799e2f5655623ed12261d
Ankit Raj <ankitraj@usf.edu>
2020-01-21 21:47:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int height;
+
+            age = 26;
+            height = 176;
+            Console.WriteLine("I am "+age+" years old, my heart rate is "+(220 - age)+" and I am "+height+" inches tall.");
+        }
+    }
+}
